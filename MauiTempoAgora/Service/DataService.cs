d9d4cc141d0226159e09e2ef3557c1aa8324e120
Usuario <Usuario@CPD03-M02>
2024-07-29 12:21:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiTempoAgora/Service/DataService.cs
@@ -0,0 +1,56 @@
+﻿using MauiTempoAgora.Models;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace MauiTempoAgora.Service
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisaoDoTempo (string cidade)
+        {
+            //https://openweathermap.org/current#current0_JSON
+            //https://home.openweathermap.org/api_keys
+
+            string appId = "6135072afe7f6cec1537d5cb08a5a1a2";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q=${cidade}&unit=metric&appid={appId}";
+
+            Tempo tempo = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode) 
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    //
+                    //
+
+                    tempo = new()
+                    {
+                        Humidity = (string)rascunho["main"]["humidity"],
+                        Temperature = (string)rascunho["main"]["temp"],
+                        Title = (string)rascunho["name"],
+                        Visibility = (string)rascunho["visibility"],
+                        Wind = (string)rascunho["wind"]["speed"],
+                        Sunrise = sunrise.ToString(),
+                        Sunset = sunset.ToString(),
+                        Weather = (string)rascunho["weather"][0]["main"],
+                        WeatherDescription = (string)rascunho["weather"][0]["description"]
+                    };
+                }
+
+
+            }
+        }
+    }
+}
